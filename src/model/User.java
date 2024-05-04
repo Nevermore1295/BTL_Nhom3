896159f8f7ab116262bc873b1f88b8e69f20c3a8
H0p3er <130352504+H0p3er@users.noreply.github.com>
2024-05-04 21:29:05 +0700

    file format

---
+++ src/model/User.java
@@ -0,0 +1,11 @@
+package model;
+
+public class User {
+    private String user_name;
+    private String user_password;
+    private String user_address;
+    private String user_phone;
+
+    private String user_fullname;
+
+}
